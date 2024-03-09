da55b67ee50a9a2563718534237003052f609a5a
Harry Lewis <harry.lewis@queensu.ca>
2024-03-09 13:40:50 -0500

    Expose GraphiQL to the public

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
 
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
