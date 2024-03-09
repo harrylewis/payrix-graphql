4e146895f06d0cdec2436bb6a1945d449028c621
Harry Lewis <harry.lewis@queensu.ca>
2024-03-09 15:45:30 -0500

    Add password field to Login type

--- app/graphql/types/login_type.rb
+++ app/graphql/types/login_type.rb
@@ -4,6 +4,7 @@ module Types
   class LoginType < BaseObject
     field :id, ID, null: false
     field :username, String, null: false
+    field :password, String, null: false
     field :email, String, null: false
   end
 end
